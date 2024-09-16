bd1c2f8d9a8ad146b51d5f674673075e44e48f8f
amolgaikwad <amol.gaikwad.31@gmail.com>
2024-09-16 16:28:08 +0530

    Define a macro

--- week_4/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ week_4/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -22,7 +22,7 @@ renamed as (
         ehail_fee,
         improvement_surcharge,
         total_amount,
-        payment_type,
+        {{ get_payment_type_description('payment_type') }} as payment_type_descripted,
         trip_type,
         congestion_surcharge
     from source
