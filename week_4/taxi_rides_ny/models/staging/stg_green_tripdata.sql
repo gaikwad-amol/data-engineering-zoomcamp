76931e66ad1bbd8dd2a6fd6a70c7bc420ef88dd7
amolgaikwad <amol.gaikwad.31@gmail.com>
2024-09-16 17:12:52 +0530

    example of using the packages which are libs

--- week_4/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ week_4/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -5,6 +5,7 @@ source as (
 
 renamed as (
     select
+        {{ dbt_utils.generate_surrogate_key(['VendorID', 'lpep_pickup_datetime']) }} as tripId,
         VendorID,
         lpep_pickup_datetime,
         lpep_dropoff_datetime,
